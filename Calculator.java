385cf887d4dd925bb5dd51834e7cf53f41e57959
Darshan <wolfmind59@gmail.com>
2025-10-23 19:44:08 +0530

    05

--- Calculator.java
+++ Calculator.java
@@ -3,32 +3,39 @@ import java.util.Scanner;
 public class Calculator {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
+          while (true) {
+               System.out.println("Enter Num 1: ");
+               int num1 = sc.nextInt();
+               System.out.println("Enter Num 2: ");
+               int num2 = sc.nextInt();
 
-          System.out.println("Enter Num 1: ");
-          int num1 = sc.nextInt();
-          System.out.println("Enter Num 2: ");
-          int num2 = sc.nextInt();
+               System.out.println("Enter Your Choice:\n + Addition\n - Subtraction * Multiplication\n / Division\n");
+               String choice = sc.next();
 
-          System.out.println("Enter Your Choice:\n + Addition\n - Subtraction * Multiplication\n / Division\n");
-          String choice = sc.next();
+               switch (choice) {
+                    case "+":
+                         System.out.println("Addition " + (num1 + num2));
+                         break;
+                    case "-":
+                         System.out.println("Subtraction " + (num1 - num2));
+                         break;
+                    case "*":
+                         System.out.println("Multiplication " + (num1 * num2));
+                         break;
+                    case "/":
+                         System.out.println("Division " + (num1 / num2));
+                         break;
+                    default:
+                         System.out.println("Invalid Input");
+               }
 
-          switch (choice) {
-               case "+":
-                    System.out.println("Addition " + (num1 + num2));
+               System.out.println("Enter Y to Continue and N to Exit");
+               String op = sc.next();
+               if (op.equals("N") || op.equals("n"))
                     break;
-               case "-":
-                    System.out.println("Subtraction " + (num1 - num2));
-                    break;
-               case "*":
-                    System.out.println("Multiplication " + (num1 * num2));
-                    break;
-               case "/":
-                    System.out.println("Division " + (num1 / num2));
-                    break;
-               default:
-                    System.out.println("Invalid Input");
 
-          sc.close();
+               sc.close();
+
           }
 
      }
