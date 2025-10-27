4fc4b9e8b543a9e30d8388358f751170ece5dbc0
Darshan <wolfmind59@gmail.com>
2025-10-27 19:18:49 +0530

    09

---
+++ EvenOdd.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+class EvenOdd {
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          while (true) {
+
+               System.out.println("Enter Number: ");
+               int num = sc.nextInt();
+
+               if (num % 2 == 0) {
+                    System.out.println("Even Number");
+               } else {
+                    System.out.println("Odd Number");
+               }
+
+               System.out.println("Enter + to continue - to Exit");
+               String op = sc.next();
+
+               if(op.equals("+")){
+                    continue;
+               }else if(op.equals("-")){
+                    break;
+               }
+
+               sc.close();
+
+          }
+     }
+}
